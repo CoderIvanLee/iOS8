7ab1fedb2e04d1d510c1957987df1b327bfedc23
经理 <jl@itcast.cn>
2016-02-18 14:04:07 +0800

    使用SSH提交代码

--- iOS8/iOS8/ViewController.m
+++ iOS8/iOS8/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"使用SSH提交代码");
 }
 
 - (void)didReceiveMemoryWarning {
